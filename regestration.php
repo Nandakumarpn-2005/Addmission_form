3a1bc73779e793aa05debab599c55bbab6b02b42
Nanda Kumar P N <nandakumar2005pn@gmail.com>
2025-10-25 15:07:53 +0530

    Update regestration.php

--- regestration.php
+++ regestration.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // PostgreSQL connection
-$conn = pg_connect("host=localhost dbname=student_addmission user=roots password=Nandakumarpn@!");
+$conn = pg_connect("host=localhost dbname=student_addmission user=nandakumarpn password=Nandakumarpn@!");
 if (!$conn) {
     die("❌ Database connection failed: " . pg_last_error());
 }
